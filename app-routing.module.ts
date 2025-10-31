9a5219e40e49ae3bc508052b7f2b160c90bb5625
Abdullah el-zohry <91378659+abdullahfarahidy@users.noreply.github.com>
2025-10-31 16:04:13 +0200

    Update import paths for Signup and Login components

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignupComponent } from 'C:\Users\ayman\OneDrive\Desktop\eventplanner-frontend\src\app\pages\signup';
-import { LoginComponent } from 'C:\Users\ayman\OneDrive\Desktop\eventplanner-frontend\src\app\pages\login';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }     
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
