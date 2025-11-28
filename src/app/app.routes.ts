5dd2b05f144134782c2ba102fb1dad2bf185fc8f
Abdullah el-zohry <91378659+abdullahfarahidy@users.noreply.github.com>
2025-11-28 23:50:32 +0200

    Refactor routes to use appRoutes and add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { EventsComponent } from './pages/events/events.component';
+import { EventDetailsComponent } from './pages/event-details/event-details.component';
+import { SearchComponent } from './pages/search/search.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+export const appRoutes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'event/:id', component: EventDetailsComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'create', component: CreateEventComponent },
 ];
